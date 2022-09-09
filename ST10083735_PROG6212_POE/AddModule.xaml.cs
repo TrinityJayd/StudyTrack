13a6603d8ae483c3f5126a19d8997b970329154d
Trinity Tiratram <ST10083735@vcconnect.edu.za>
2022-09-09 22:32:13 +0200

    Designed add module and semester details page and completed naviagtion.

---
+++ ST10083735_PROG6212_POE/AddModule.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ST10083735_PROG6212_POE
+{
+    /// <summary>
+    /// Interaction logic for AddModule.xaml
+    /// </summary>
+    public partial class AddModule : UserControl
+    {
+        public event EventHandler HideModulePageButtonClicked;
+        public AddModule()
+        {
+            InitializeComponent();
+        }
+
+        private void completebtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (HideModulePageButtonClicked != null)
+                HideModulePageButtonClicked(this, EventArgs.Empty);
+        }
+    }
+}
--- ST10083735_PROG6212_POE/MainWindow.xaml.cs
+++ ST10083735_PROG6212_POE/MainWindow.xaml.cs
@@ -6,9 +6,7 @@ using System.Windows.Media;
 
 namespace ST10083735_PROG6212_POE
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
 
@@ -20,6 +18,20 @@ namespace ST10083735_PROG6212_POE
             //Author:andreask
             //https://stackoverflow.com/questions/25585491/showing-user-control-from-another-user-controls-button-click-in-main-window
             landingPage.HideButtonClicked += OnHideButtonClicked;
+            addModule.HideModulePageButtonClicked += OnHideModulePageButtonClicked;
+            semesterDetails.HideSemesterDetailsButtonClicked += OnHideSemesterDetailsButtonClicked;
+        }
+
+        private void OnHideSemesterDetailsButtonClicked(object? sender, EventArgs e)
+        {
+            semesterDetails.Visibility = Visibility.Collapsed;
+            home.Visibility = Visibility.Visible;
+        }
+
+        private void OnHideModulePageButtonClicked(object? sender, EventArgs e)
+        {
+            addModule.Visibility = Visibility.Collapsed;
+            semesterDetails.Visibility = Visibility.Visible;
         }
 
         //Author:andreask
@@ -27,7 +39,7 @@ namespace ST10083735_PROG6212_POE
         private void OnHideButtonClicked(object? sender, EventArgs e)
         {
             landingPage.Visibility = Visibility.Collapsed;
-            home.Visibility = Visibility.Visible;
+            addModule.Visibility = Visibility.Visible;
         }
 
         //youtube reference
@@ -35,6 +47,8 @@ namespace ST10083735_PROG6212_POE
         {
             landingPage.Visibility = Visibility.Collapsed;
             home.Visibility = Visibility.Collapsed;
+            addModule.Visibility = Visibility.Collapsed;
+            semesterDetails.Visibility = Visibility.Collapsed;
 
             control.Visibility = Visibility.Visible;
 
@@ -42,9 +56,7 @@ namespace ST10083735_PROG6212_POE
 
         private void homebtn_Click(object sender, RoutedEventArgs e)
         {
-            SetActiveUserControl(home);
-           
-            
+            SetActiveUserControl(home);            
         }
 
         private void mainLb_MouseDown(object sender, MouseButtonEventArgs e)
