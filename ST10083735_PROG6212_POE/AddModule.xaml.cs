e87a7c0d64790294b7d34956341b9073df764929
Trinity Tiratram <ST10083735@vcconnect.edu.za>
2022-09-18 20:00:00 +0200

    Converted textboxes to spin edits

--- ST10083735_PROG6212_POE/AddModule.xaml.cs
+++ ST10083735_PROG6212_POE/AddModule.xaml.cs
@@ -59,9 +59,7 @@ namespace ST10083735_PROG6212_POE
             errorlb.Visibility = Visibility.Collapsed;
 
             
-            if (String.IsNullOrWhiteSpace(moduleCodetbx.Text) || String.IsNullOrWhiteSpace(moduleNametbx.Text) 
-                || String.IsNullOrWhiteSpace(numCreditstbx.Text) || String.IsNullOrWhiteSpace(classHourstbx.Text)
-                || String.IsNullOrWhiteSpace(numWeekstb.Text) || String.IsNullOrWhiteSpace(datedp.SelectedDate.ToString()))
+            if (String.IsNullOrWhiteSpace(moduleCodetbx.Text) || String.IsNullOrWhiteSpace(moduleNametbx.Text) || String.IsNullOrWhiteSpace(datedp.SelectedDate.ToString()))
             {
                 errorlb.Visibility = Visibility.Visible;
             }
@@ -78,9 +76,9 @@ namespace ST10083735_PROG6212_POE
                     errorlb.Content = "Missing Information. All fields are required.";
                     string moduleCode = moduleCodetbx.Text;
                     string moduleName = moduleNametbx.Text;
-                    double classHours = Convert.ToDouble(classHourstbx.Text);
-                    double credits = Convert.ToDouble(numCreditstbx.Text);
-                    double weeks = Convert.ToDouble(numWeekstb.Text);
+                    double classHours = Convert.ToDouble(hoursspn.Value);
+                    double credits = Convert.ToDouble(creditspn.Value;
+                    double weeks = Convert.ToDouble(weeksspn.Value);
                     DateTime startdate = datedp.SelectedDate.Value;
 
                     moduleList.Add(new Module(moduleCode, moduleName, credits, classHours, weeks, startdate));
@@ -94,7 +92,7 @@ namespace ST10083735_PROG6212_POE
 
                     completebtn.Visibility = Visibility.Visible;
                     numberOfWeekslb.Visibility = Visibility.Collapsed;
-                    numWeekstb.Visibility = Visibility.Collapsed;
+                    weeksspn.Visibility = Visibility.Collapsed;
                     startDatelb.Visibility = Visibility.Collapsed;
                     datedp.Visibility = Visibility.Collapsed;
                 }
@@ -129,31 +127,14 @@ namespace ST10083735_PROG6212_POE
         {
             moduleCodetbx.Clear();
             moduleNametbx.Clear();
-            numCreditstbx.Clear();
-            classHourstbx.Clear();
-            
+            creditspn.Value = 8;
+            hoursspn.Value = 4;
+
         }
 
         
 
-        //https://iditect.com/guide/csharp/csharp_howto_make_a_textbox_only_accept_numbers_in_wpf.html#:~:text=C%23%20How%20to%20make%20a%20TextBox%20only%20accept,into%20the%20textbox%20but%20only%20typing%20numeric%20input.
-        private void TypeNumericValidation(object sender, TextCompositionEventArgs e)
-        {
-            e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
-        }
-
-        private void PasteNumericValidation(object sender, DataObjectPastingEventArgs e)
-        {
-            if (e.DataObject.GetDataPresent(typeof(String)))
-            {
-                String input = (String)e.DataObject.GetData(typeof(String));
-                if (new Regex("[^0-9]+").IsMatch(input))
-                {
-                    e.CancelCommand();
-                }
-            }
-            else e.CancelCommand();
-        }
+        
 
        
     }
