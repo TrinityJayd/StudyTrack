8b8c0f172af5d11fc2bc3ff839239c817f9ffcf6
Trinity Tiratram <ST10083735@vcconnect.edu.za>
2022-10-18 13:37:40 +0200

    Design changes. Added login and regisater methods to user class.

--- Modules/User.cs
+++ Modules/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,5 +16,44 @@ namespace Modules
         public string ContactNumber { get; set; }
         public string Password { get; set; }
         public string Salt { get; set; }
+
+        public string HashPassword(string password)
+        {
+            SHA256 hash = SHA256.Create();
+            var passwordBytes = Encoding.Default.GetBytes(password);
+            var hashedPassword = hash.ComputeHash(passwordBytes);
+            return Convert.ToHexString(hashedPassword);
+        }
+
+        public void Login(string username, string password)
+        {
+            //using AppDataContext appDataContext = new AppDataContext();
+            //var userFound = appDataContext.Users.Any(u => u.Name.Equals(username));
+
+            //if (userFound == true)
+            //{
+            //    var loginUser = appDataContext.Users.FirstOrDefault(u => u.Name.Equals(username));
+            //    if (HashPassword($"{password}{loginUser.Salt}").Equals(loginUser.Password) == true)
+            //    {
+            //        MessageBox.Show("Logged in");
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("Not logged in");
+            //    }
+            //}
+        }
+
+        public void Register(string username, string password)
+        {
+         
+            //using AppDataContext appDataContext = new AppDataContext();
+
+            //var salt = DateTime.Now.ToString();
+            //var hashedPassword = HashPassword($"{password}{salt}");
+            //appDataContext.Add(new User() { Name = username, Password = hashedPassword, Salt = salt });
+            //appDataContext.SaveChangesAsync();
+            //MessageBox.Show("Registered");
+        }
     }
 }
