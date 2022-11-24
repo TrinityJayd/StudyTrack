f969f26e00ec3206e284ef8e035788abcddbbaf2
Trinity Tiratram <ST10083735@vcconnect.edu.za>
2022-11-24 14:03:27 +0200

    Added home page components

--- POE/Controllers/UsersController.cs
+++ POE/Controllers/UsersController.cs
@@ -6,6 +6,7 @@ namespace POE.Controllers
 {
     public class UsersController : Controller
     {
+        
         private readonly Prog6212P2Context _context;
 
         public UsersController(Prog6212P2Context context)
@@ -16,7 +17,7 @@ namespace POE.Controllers
         // GET: Users/Create
         public IActionResult Create()
         {
-            return View();
+            return View();           
         }
 
         // POST: Users/Create
@@ -28,13 +29,55 @@ namespace POE.Controllers
         {
             if (ModelState.IsValid)
             {
-                Account signUp = new Account();
-                await signUp.Register(user);
-                return RedirectToAction(nameof(Index));
-            }
-            return View(user);
-        }
+                Account account = new Account();
+                ValidationMethods validation = new ValidationMethods();
+                if (!validation.OnlyLetters(user.Name))
+                {
+                    ModelState.AddModelError("Name", "Name must only contain letters.");
 
+                }
+                //check if the last name is valid
+                else if (!validation.OnlyLetters(user.Lastname))
+                {
+                    ModelState.AddModelError("Lastname", "Last name must only contain letters.");
+
+                }
+                else if (account.UsernameExists(user.Username))
+                {
+                    ModelState.AddModelError("Username", "Username already exists.");
+
+                }
+                else if (!validation.passwordRequirements(user.Password))
+                {
+                    //Code Attribution
+                    //Author:CodeNotFound
+                    //Link:https://stackoverflow.com/questions/43561214/display-error-message-on-the-view-from-controller-asp-net-mvc-5
+                    ModelState.AddModelError("Password", "Password must contain at least 8 characters, 1 uppercase, 1 lowercase, 1 number and 1 special character.");
+                }
+                else if (!validation.IsPhoneNumberValid(user.CellNumber))
+                {
+                    ModelState.AddModelError("CellNumber", "Cell number must be numeric and begin with 0.");
+
+                }
+                else if (account.PhoneExists(user.CellNumber))
+                {
+                    ModelState.AddModelError("CellNumber", "User with this cell number already exists.");
+                }
+                else if (account.EmailExists(user.Email))
+                {
+                    ModelState.AddModelError("Email", "User with this email already exists.");
+
+                }
+                else
+                {
+                    await account.Register(user);
+                    return RedirectToAction(nameof(Index));
+                }
+
+                return View();
+            }
+            return View();
+        }
         public IActionResult Login()
         {
             return View();
@@ -45,7 +88,7 @@ namespace POE.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Login([Bind("Username,Password")] string username,string password)
+        public async Task<IActionResult> Login([Bind("Username,Password")] string username, string password)
         {
             if (ModelState.IsValid)
             {
--- POE/wwwroot/js/site.js
+++ POE/wwwroot/js/site.js
@@ -1,4 +1,10 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿//Code Attribution
+//Author:aelor
+//Link: https://stackoverflow.com/questions/21727317/how-to-check-confirm-password-field-in-form-without-reloading-page
+$('#password, #confirm_password').on('keyup', function () {
+    if ($('#password').val() == $('#confirm_password').val()) {
+        $('#message').html('');
+    } else {
+        $('#message').html('Passwords do not match.');
+    }       
+});
